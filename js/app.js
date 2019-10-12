2f65ce99ab4f26b206dc135227abe31e8dd501bb
KevCB1988 <kevcb1988@gmail.com>
2019-10-12 14:50:23 -0500

    Proyecto finalizado

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const miWeb = new fullpage('#fullpage', {
 		 fitToSection: false, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
 		 fitToSectionDelay: 300, // Delay antes de acomodar la seccion automaticamente.
 		 easing: 'easeInOutCubic', // Funcion de tiempo de la animacion.
-		 scrollingSpeed: 700, // Velocidad del scroll. Valores: en milisegundos.
+		 scrollingSpeed: 500, // Velocidad del scroll. Valores: en milisegundos.
 		 css3: true, // Si usara CSS3 o javascript.
 		 easingcss3: 'ease-out', // Curva de velocidad del efecto.
          loopBottom: false, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
@@ -22,10 +22,15 @@ const miWeb = new fullpage('#fullpage', {
 		 anchors: ['inicio', 'productos', 'contacto'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
 		 navigationTooltips: ['Inicio', 'Productos', 'Contacto'], // Tooltips que mostrara por cada boton.
          showActiveTooltip: false, // Mostrar tooltip activa.
-         	//   :::::: Secciones
+	//   :::::: Secciones
 	// ──────────────────────────────────────────────────
 		 sectionsColor : ['#000', '#c2c2c2', '#000'], // Color de fondo de cada seccion.
 		 verticalCentered: true, // Si alineara de forma vertical los contenidos de cada seccion.
 	// ──────────────────────────────────────────────────
+	//   :::::: Slides
+	// ──────────────────────────────────────────────────
+	controlArrows: false, // Flechas del slide
+	slidesNavigation: true, // Indicadores del slide
+// ──────────────────────────────────────────────────
 });
 
